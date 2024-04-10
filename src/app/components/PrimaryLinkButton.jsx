fe20044370983735d89c8a673ff18f9eb0f1ca80
gabewolford <gabewolford@gmail.com>
2024-04-10 08:39:06 -0700

    increased font weight, adjusted color and hover color

--- src/app/components/PrimaryLinkButton.jsx
+++ src/app/components/PrimaryLinkButton.jsx
@@ -4,7 +4,7 @@ export default function PrimaryLinkButton({ linkTo, buttonText }) {
   return (
     <Link
       href={linkTo}
-      className="uppercase bg-clutchBlue-500 hover:bg-clutchBlue-400 transition duration-300 text-white min-w-fit min-h-fit px-6 py-3"
+      className="uppercase bg-clutchBlue-600 hover:bg-clutchBlue-500 transition duration-300 text-white font-medium min-w-fit min-h-fit px-6 py-3"
     >
       {buttonText}
     </Link>
