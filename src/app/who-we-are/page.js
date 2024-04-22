4be92fd2c2f749be82c05c1a4965d2029dd60c2b
gabewolford <gabewolford@gmail.com>
2024-04-22 09:31:54 -0700

    added section to who we are page

--- src/app/who-we-are/page.js
+++ src/app/who-we-are/page.js
@@ -1,11 +1,23 @@
+import PhotoLeftTextRightSection from "../components/PhotoLeftTextRightSection";
+import teamPhoto from "../../../public/images/clutch-team-photo.png";
+
 export default function WhoWeArePage() {
   return (
     <main>
       <section id="history" className="border h-svh">
         History Section
       </section>
-      <section id="team" className="border h-svh">
-        Team Section
+      <PhotoLeftTextRightSection
+        id="team"
+        photoUrl={teamPhoto}
+        altText="Team photo"
+        heading="Who We Are"
+        paragraph="Here at Clutch Industries, we have an unwavering commitment to great service and quality in every thing that we do. Meet the team that makes it all possible."
+        buttonText="Meet the team"
+        linkTo="/who-we-are/#team-members"
+      />
+      <section id="team-members" className="border h-svh">
+        History Section
       </section>
     </main>
   );
