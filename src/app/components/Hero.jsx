bfdfe2f564e6023ddf09dcb432449f5c26550d5d
gabewolford <gabewolford@gmail.com>
2024-04-23 10:52:07 -0700

    dried up h1 code

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -14,43 +14,7 @@ export default function Hero() {
       />
       <div className="absolute z-30 w-full h-svh flex justify-start p-5 lg:p-10 items-end">
         <div className="flex flex-col gap-4 animate__animated animate__fadeInUp text-white">
-          <h1 className="flex md:hidden text-3xl leading-normal font-medium">
-            Your full service
-            <br />
-            construction experts
-            <br />
-            in the Willamette Valley
-            <br />& beyond.
-          </h1>
-
-          <h1 className="hidden md:flex lg:hidden text-6xl leading-normal">
-            Your full service
-            <br />
-            construction experts
-            <br />
-            in the Willamette Valley
-            <br />& beyond.
-          </h1>
-
-          <h1 className="hidden lg:flex xl:hidden text-7xl leading-tight">
-            Your full service
-            <br />
-            construction experts
-            <br />
-            in the Willamette Valley
-            <br />& beyond.
-          </h1>
-
-          <h1 className="hidden xl:flex 2xl:hidden text-7xl leading-[1.1]">
-            Your full service
-            <br />
-            construction experts
-            <br />
-            in the Willamette Valley
-            <br />& beyond.
-          </h1>
-
-          <h1 className="hidden 2xl:flex text-8xl leading-tight">
+          <h1 className="flex text-3xl font-medium leading-normal md:font-normal md:text-6xl md:leading-normal lg:text-6xl lg:leading-tight xl:text-7xl xl:leading-[1.1] 2xl:text-8xl 2xl:leading-tight">
             Your full service
             <br />
             construction experts
