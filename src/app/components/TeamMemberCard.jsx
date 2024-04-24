0952e5eb33e4ef565789da2276dc344b7ba1928f
gabewolford <gabewolford@gmail.com>
2024-04-24 10:06:10 -0700

    updated team cards across all breakpoints

--- src/app/components/TeamMemberCard.jsx
+++ src/app/components/TeamMemberCard.jsx
@@ -10,18 +10,18 @@ export default function TeamMemberCard({ fullName, title, email, photoUrl }) {
         className="w-full h-auto object-cover"
       />
       <div className="absolute flex flex-col gap-0.5 bottom-0 left-0 bg-clutchBlue-900/80 backdrop-blur-md w-full h-fit px-4 py-4">
-        <div className="flex flex-row items-center justify-between text-white text-xl">
+        <div className="flex flex-row items-center justify-between text-white text-sm md:text-base lg:text-xl">
           <p className="font-medium">{fullName}</p>
           <a
             href={`mailto:${email}`}
             target="_blank"
             aria-label="Contact by email"
           >
-            <MdOutlineEmail className="h-full w-auto text-2xl" />
+            <MdOutlineEmail className="h-full w-auto lg:text-2xl" />
           </a>
         </div>
 
-        <p className="text-clutchBlue-100">{title}</p>
+        <p className="text-clutchBlue-100 text-xs md:text-sm">{title}</p>
       </div>
     </>
   );
