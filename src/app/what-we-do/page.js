ce0a6158655e42481ec48da9b40c54a42822ac68
gabewolford <gabewolford@gmail.com>
2024-05-02 11:06:29 -0700

    added placeholder sections for services page

--- src/app/what-we-do/page.js
+++ src/app/what-we-do/page.js
@@ -1,44 +1,48 @@
-import ServicesDrawer from "../components/ServicesDrawer";
 import TextAndLinkSection from "../components/TextAndLinkSection";
-import DrawerSection from "../components/DrawerSection";
+import PhotoLeftTextRightSection from "../components/PhotoLeftTextRightSection";
+import PhotoRightTextLeftSection from "../components/PhotoRightTextLeftSection";
+import placeholder from "../../../public/images/placeholder.png";
 
 export default function WhatWeDoPage() {
   return (
     <main>
-      <DrawerSection id="what-we-do" heading="Our Services" variant="light">
-        <ServicesDrawer
-          service="Construction"
-          description="We focus on adaptive reuse, flex warehouse, and full multi-family construction from land acquisition to occupancy in the Willamette Valley and the surrounding area. We currently have more than 500 multifamily units planned over the next three years."
-          linkTo="#construction"
-          variant="dark"
-          buttonText="Learn more"
+      <div className="flex flex-col h-full pt-10 lg:pt-20">
+        <section className="w-full px-5 lg:px-10">
+          <h2 className="text-4xl md:text-7xl 2xl:text-8xl">What We Do</h2>
+        </section>
+        <PhotoLeftTextRightSection
+          id="construction"
+          photoUrl={placeholder}
+          altText="construction"
+          heading="Construction"
+          paragraph="We focus on adaptive reuse, flex warehouse, and full multi-family construction from land acquisition to occupancy in the Willamette Valley and the surrounding area. We currently have more than 500 multifamily units planned over the next three years."
+          buttonText="View Projects"
+          linkTo="/projects"
+          variant="light"
         />
-        <ServicesDrawer
-          service="Investment"
-          description="We are always excited about finding our next project, no matter the size. We look forward to more investment opportunities and potential partners, projects, and development within our community."
-          linkTo="#investment"
+        <PhotoRightTextLeftSection
+          id="investment"
+          photoUrl={placeholder}
+          altText="investment"
+          heading="Investment"
+          paragraph="We are always excited about finding our next project, no matter the size. We look forward to more investment opportunities and potential partners, projects, and development within our community."
+          buttonText="View Projects"
+          linkTo="/projects"
           variant="dark"
-          buttonText="Learn more"
         />
-        <ServicesDrawer
-          service="Management"
-          description="We are dedicated to providing personalized and attentive care for each property under our management, fostering long-term tenant satisfaction and property value appreciation."
-          linkTo="#management"
-          variant="dark"
-          buttonText="Learn more"
+        <PhotoLeftTextRightSection
+          id="management"
+          photoUrl={placeholder}
+          altText="management"
+          heading="Management"
+          paragraph="We are dedicated to providing personalized and attentive care for each property under our management, fostering long-term tenant satisfaction and property value appreciation."
+          buttonText="View Projects"
+          linkTo="/projects"
+          variant="light"
         />
-      </DrawerSection>
-      <section id="construction" className="border h-svh">
-        Construction Section
-      </section>
-      <section id="investment" className="border h-svh">
-        Investment Section
-      </section>
-      <section id="management" className="border h-svh">
-        Management Section
-      </section>
+      </div>
       <TextAndLinkSection
-        heading="Ready to work with us?"
+        heading="Want to work with us?"
         buttonText="Get in touch"
         variant="dark"
         purpose="contact"
