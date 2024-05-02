aee06faadc242c95d057496c534f7d41e27af966
gabewolford <gabewolford@gmail.com>
2024-05-02 11:13:28 -0700

    tweaked layout and variants of services page

--- src/app/what-we-do/page.js
+++ src/app/what-we-do/page.js
@@ -1,46 +1,66 @@
 import TextAndLinkSection from "../components/TextAndLinkSection";
 import PhotoLeftTextRightSection from "../components/PhotoLeftTextRightSection";
 import PhotoRightTextLeftSection from "../components/PhotoRightTextLeftSection";
-import placeholder from "../../../public/images/placeholder.png";
+import placeholder from "../../../public/images/clutch-team-photo.png";
+import DrawerSection from "../components/DrawerSection";
+import ServicesDrawer from "../components/ServicesDrawer";
 
 export default function WhatWeDoPage() {
   return (
     <main>
-      <div className="flex flex-col h-full pt-10 lg:pt-20">
-        <section className="w-full px-5 lg:px-10">
-          <h2 className="text-4xl md:text-7xl 2xl:text-8xl">What We Do</h2>
-        </section>
-        <PhotoLeftTextRightSection
-          id="construction"
-          photoUrl={placeholder}
-          altText="construction"
-          heading="Construction"
-          paragraph="We focus on adaptive reuse, flex warehouse, and full multi-family construction from land acquisition to occupancy in the Willamette Valley and the surrounding area. We currently have more than 500 multifamily units planned over the next three years."
-          buttonText="View Projects"
-          linkTo="/projects"
+      <DrawerSection id="services" heading="Our Services" variant="light">
+        <ServicesDrawer
+          service="Construction"
+          description="We focus on adaptive reuse, flex warehouse, and full multi-family construction from land acquisition to occupancy in the Willamette Valley and the surrounding area. We currently have more than 500 multifamily units planned over the next three years."
+          linkTo="#construction"
           variant="light"
+          buttonText="Learn more"
         />
-        <PhotoRightTextLeftSection
-          id="investment"
-          photoUrl={placeholder}
-          altText="investment"
-          heading="Investment"
-          paragraph="We are always excited about finding our next project, no matter the size. We look forward to more investment opportunities and potential partners, projects, and development within our community."
-          buttonText="View Projects"
-          linkTo="/projects"
-          variant="dark"
-        />
-        <PhotoLeftTextRightSection
-          id="management"
-          photoUrl={placeholder}
-          altText="management"
-          heading="Management"
-          paragraph="We are dedicated to providing personalized and attentive care for each property under our management, fostering long-term tenant satisfaction and property value appreciation."
-          buttonText="View Projects"
-          linkTo="/projects"
+        <ServicesDrawer
+          service="Investment"
+          description="We are always excited about finding our next project, no matter the size. We look forward to more investment opportunities and potential partners, projects, and development within our community."
+          linkTo="#investment"
           variant="light"
+          buttonText="Learn more"
         />
-      </div>
+        <ServicesDrawer
+          service="Management"
+          description="We are dedicated to providing personalized and attentive care for each property under our management, fostering long-term tenant satisfaction and property value appreciation."
+          linkTo="#management"
+          variant="light"
+          buttonText="Learn more"
+        />
+      </DrawerSection>
+      <PhotoLeftTextRightSection
+        id="construction"
+        photoUrl={placeholder}
+        altText="construction"
+        heading="Construction"
+        paragraph="We focus on adaptive reuse, flex warehouse, and full multi-family construction from land acquisition to occupancy in the Willamette Valley and the surrounding area. We currently have more than 500 multifamily units planned over the next three years."
+        buttonText="View Projects"
+        linkTo="/projects"
+        variant="dark"
+      />
+      <PhotoRightTextLeftSection
+        id="investment"
+        photoUrl={placeholder}
+        altText="investment"
+        heading="Investment"
+        paragraph="We are always excited about finding our next project, no matter the size. We look forward to more investment opportunities and potential partners, projects, and development within our community."
+        buttonText="View Projects"
+        linkTo="/projects"
+        variant="light"
+      />
+      <PhotoLeftTextRightSection
+        id="management"
+        photoUrl={placeholder}
+        altText="management"
+        heading="Management"
+        paragraph="We are dedicated to providing personalized and attentive care for each property under our management, fostering long-term tenant satisfaction and property value appreciation."
+        buttonText="View Projects"
+        linkTo="/projects"
+        variant="dark"
+      />
       <TextAndLinkSection
         heading="Want to work with us?"
         buttonText="Get in touch"
