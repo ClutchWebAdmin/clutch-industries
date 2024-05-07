4a136190966d783d2f965c51e29c7970028ccb89
gabewolford <gabewolford@gmail.com>
2024-05-07 12:42:27 -0700

    stats section now rendering server side

--- src/app/components/StatsSection.jsx
+++ src/app/components/StatsSection.jsx
@@ -1,77 +1,40 @@
-"use client";
-
-import { useState, useEffect } from "react";
-import { useInView } from "react-intersection-observer";
 import { oxanium } from "../styles/fonts";
+import { client } from "../../../sanity/lib/client";
 
-const maxProjectsCompleted = 8;
-const maxSquareFootage = 321456;
-const maxPlaceholder = 54320;
-
-export default function StatsSection() {
-  const [projectsCompleted, setProjectsCompleted] = useState(0);
-  const [squareFootage, setSquareFootage] = useState(0);
-  const [placeholder, setPlaceholder] = useState(0);
-  const { ref, inView } = useInView({
-    triggerOnce: true, // Only trigger once
-    threshold: 0.5, // Trigger when 50% of the component is in view
-  });
-
-  useEffect(() => {
-    if (inView) {
-      const projectsInterval = setInterval(() => {
-        // Increment projectsCompleted by 1 until it reaches maxProjectsCompleted
-        if (projectsCompleted < maxProjectsCompleted) {
-          setProjectsCompleted((prevCount) => prevCount + 1);
-        }
-      }, 200);
-
-      return () => {
-        clearInterval(projectsInterval);
-      };
-    }
-  }, [inView, projectsCompleted]);
-
-  useEffect(() => {
-    if (inView) {
-      const squareFootageInterval = setInterval(() => {
-        // Increment squareFootage by 100 until it reaches maxSquareFootage
-        if (squareFootage < maxSquareFootage) {
-          setSquareFootage((prevCount) => prevCount + 100);
-        }
-      }, 1);
-
-      return () => {
-        clearInterval(squareFootageInterval);
-      };
-    }
-  }, [inView, squareFootage]);
-
-  useEffect(() => {
-    if (inView) {
-      const placeholderInterval = setInterval(() => {
-        // Increment placeholder by 50 until it reaches maxPlaceholder
-        if (placeholder < maxPlaceholder) {
-          setPlaceholder((prevCount) => prevCount + 50);
-        }
-      }, 5);
-
-      return () => {
-        clearInterval(placeholderInterval);
-      };
-    }
-  }, [inView, placeholder]);
+export default async function StatsSection() {
+  let projectsCompleted;
+  let squareFootage;
+  let totalUnits;
 
   const formatNumberWithCommas = (number) => {
     return number.toLocaleString();
   };
 
+  const data = await client.fetch(`*[_type == "projects"]{
+        'squareFootage': projectDetails.squareFootage,
+        'unitsObject': projectDetails.units
+      }`);
+
+  // Calculate the total number of projects
+  projectsCompleted = data.length;
+
+  // Calculate the sum of square footage
+  squareFootage = data.reduce((acc, project) => acc + project.squareFootage, 0);
+
+  // Calculate the total number of units
+  totalUnits = data.reduce((acc, project) => {
+    const units = Object.values(project.unitsObject);
+    return acc + units.reduce((unitAcc, unit) => unitAcc + unit, 0);
+  }, 0);
+
   return (
-    <section
-      ref={ref}
-      className="grid grid-cols-1 lg:grid-cols-3 text-white h-fit"
-    >
-      <div className="bg-clutchBlue-800 col-span-1 flex flex-row">
+    <section className="grid grid-cols-1 lg:grid-cols-3 text-white h-fit">
+      <div
+        className="bg-clutchBlue-800 col-span-1 flex flex-row"
+        data-aos="flip-right"
+        data-aos-duration="1500"
+        data-aos-once="true"
+      >
         <div className="flex w-8 bg-gray-800"></div>
         <div className=" flex flex-col justify-between w-full">
           <div className="flex flex-grow flex-col gap-2 px-4 py-6 justify-center ">
@@ -79,11 +42,7 @@ export default function StatsSection() {
               Projects Completed
             </h3>
             <p className="text-6xl">
-              {formatNumberWithCommas(
-                projectsCompleted >= maxProjectsCompleted
-                  ? maxProjectsCompleted
-                  : projectsCompleted
-              )}
+              {formatNumberWithCommas(projectsCompleted)}
             </p>
           </div>
 
@@ -91,38 +50,38 @@ export default function StatsSection() {
         </div>
       </div>
 
-      <div className="bg-clutchBlue-700 col-span-1 flex flex-row">
+      <div
+        className="bg-clutchBlue-700 col-span-1 flex flex-row"
+        data-aos="flip-right"
+        data-aos-duration="1500"
+        data-aos-once="true"
+      >
         <div className="flex w-8 bg-gray-600"></div>
         <div className=" flex flex-col justify-between w-full">
           <div className="flex flex-grow flex-col gap-2 px-4 py-6 justify-center ">
             <h3 className={`${oxanium.className} text-lg font-semibold`}>
               Square Footage
             </h3>
-            <p className="text-6xl">
-              {formatNumberWithCommas(
-                squareFootage >= maxSquareFootage
-                  ? maxSquareFootage
-                  : squareFootage
-              )}
-            </p>
+            <p className="text-6xl">{formatNumberWithCommas(squareFootage)}</p>
           </div>
 
           <div className="h-2 w-full bg-gray-500"></div>
         </div>
       </div>
 
-      <div className="bg-clutchBlue-600 col-span-1 flex flex-row">
+      <div
+        className="bg-clutchBlue-600 col-span-1 flex flex-row"
+        data-aos="flip-right"
+        data-aos-duration="1500"
+        data-aos-once="true"
+      >
         <div className="flex w-8 bg-gray-400"></div>
         <div className=" flex flex-col justify-between w-full">
           <div className="flex flex-grow flex-col gap-2 px-4 py-6 justify-center ">
             <h3 className={`${oxanium.className} text-lg font-semibold`}>
-              Placeholder
+              Total Units
             </h3>
-            <p className="text-6xl">
-              {formatNumberWithCommas(
-                placeholder >= maxPlaceholder ? maxPlaceholder : placeholder
-              )}
-            </p>
+            <p className="text-6xl">{formatNumberWithCommas(totalUnits)}</p>
           </div>
 
           <div className="h-2 w-full bg-gray-300"></div>
