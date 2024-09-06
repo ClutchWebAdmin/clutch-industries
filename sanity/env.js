5e3428bf8420a0e1f773d68dafbeaf1b67d3c0e7
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2024-09-06 10:38:02 -0700

    fix

--- sanity/env.js
+++ sanity/env.js
@@ -1,12 +1,6 @@
-// export const apiVersion =
-//   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-04-29'
-
-// export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-// export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-// export const useCdn = false
-
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-06-14'
-export const dataset = 'production';
-export const projectId = 'inl96r16';
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-04-29'
+
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const useCdn = false
