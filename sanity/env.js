295b827f9426698fd8aacf1c333fc3b2a8e9edef
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2024-09-09 09:01:26 -0700

    res test 1

--- sanity/env.js
+++ sanity/env.js
@@ -4,5 +4,5 @@ export const apiVersion =
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-
 export const useCdn = false
+
